3d53ebb0723a161c300c181cba824454e084ce89
MaxBrohman <maksimbrochman@gmail.com>
2019-05-18 16:36:58 +0300

    build folder replaced with docs

---
+++ docs/precache-manifest.fbb264920c5d50d5ca0b5473a3fae153.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc32f62382555c55d8908b9fb7663d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "522a9977cb66cd4b348c",
+    "url": "/static/css/main.0dca0152.chunk.css"
+  },
+  {
+    "revision": "8c0cae1e53e0dde7a123",
+    "url": "/static/js/2.2d662d09.chunk.js"
+  },
+  {
+    "revision": "522a9977cb66cd4b348c",
+    "url": "/static/js/main.134ff43c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
